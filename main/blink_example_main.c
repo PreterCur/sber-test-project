368b8fc1219147d9620d8445f711740ada6f6036
PreterCur <42983734+PreterCur@users.noreply.github.com>
2026-06-25 21:32:38 +0300

    button and led skeleton
    defined task roles, made working button task. led task has logs instead of led commands because ESP32CAM board does not have RGB LED onboard. Will change when i'd get a proper dev board

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -11,96 +11,74 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "sdkconfig.h"
+#include "led_strip.h"
 
 #include "esp_timer.h"
+
+#include "sdkconfig.h"
 
 
 static const char *INIT_TAG = "INIT";
 static const char *BTN_TAG = "BTN";
+static const char *LED_TAG = "LED";
 static const char *WIFI_TAG = "WIFI";
 static const char *ADC_TAG = "ADC";
 
-static const char version_string[] = "Firmware version: v1.0.0";
+static const char *ERROR_TAG = "ERROR";
+
+
+static const char version_string[] = "Firmware version: v1.0.2";
 
 static TaskHandle_t button_task_h = NULL;
+static TaskHandle_t led_task_h = NULL;
+static TaskHandle_t measure_task_h = NULL;
 
-typedef struct
+
+/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+static uint8_t s_led_state = 0;
+
+
+static void blink_led(void)
 {
-    TaskHandle_t *btn_task;
-    uint32_t exti_pin;
-    uint32_t pin_logic_level;
-    uint64_t debounce_period;
-    uint64_t last_isr_timestamp;
-    uint64_t press_start_timestamp;
-    uint64_t press_end_timestamp;
-    uint64_t long_press_period;
-    uint64_t short_press_period;
-    uint64_t last_press_length;
-}button_exti_str;
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
 
-#define BTN_NOTIFY_LONG_PRESS   (uint32_t)1
-#define BTN_NOTIFY_SHORT_PRESS  (uint32_t)2
-
-// ISR handler function
-static void IRAM_ATTR gpio_isr_handler(void* arg) 
+static void configure_led(void)
 {
-    if (arg == NULL)
-    {
-        ESP_LOGE(BTN_TAG, "NULL arg to gpio isr handler\r\n");
-        return;
-    }
-
-    button_exti_str *btn_p = (button_exti_str *)arg;
-    uint64_t now = esp_timer_get_time() / 1000;
-    if (now - btn_p->last_isr_timestamp > btn_p->debounce_period)
-    {
-        btn_p->last_isr_timestamp = now;
-        int level = gpio_get_level(btn_p->exti_pin);
-        if (level != btn_p->pin_logic_level)
-        {
-            //button pushed
-            btn_p->press_start_timestamp = now;
-        }
-        else
-        {
-            //button released
-            btn_p->press_end_timestamp = now;
-            uint64_t press_length = btn_p->press_start_timestamp - btn_p->press_end_timestamp;
-            btn_p->last_press_length = press_length;
-            if (press_length > btn_p->short_press_period &&
-                press_length < btn_p->long_press_period)
-            {
-                BaseType_t higher_priority_task_woken = pdFALSE;
-                xTaskNotifyFromISR(*btn_p->btn_task, 
-                                    BTN_NOTIFY_SHORT_PRESS, 
-                                    eSetBits, 
-                                    &higher_priority_task_woken);
-                btn_p->last_isr_timestamp = now;
-                if (higher_priority_task_woken) 
-                {
-                    portYIELD_FROM_ISR();
-                }
-            }
-            else if (press_length >= btn_p->long_press_period)
-            {
-                BaseType_t higher_priority_task_woken = pdFALSE;
-                xTaskNotifyFromISR(*btn_p->btn_task, 
-                                    BTN_NOTIFY_LONG_PRESS, 
-                                    eSetBits, 
-                                    &higher_priority_task_woken);
-                btn_p->last_isr_timestamp = now;
-                if (higher_priority_task_woken) 
-                {
-                    portYIELD_FROM_ISR();
-                }
-            }
-        }
-    }
-    // Notify task or set flag here
+    ESP_LOGI(INIT_TAG, "Example configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
 
 
+typedef struct
+{
+    gpio_num_t btn_pin;
+    uint32_t pin_logic_level;
+    uint32_t debounce_samples;
+    uint32_t polling_period_ms;
+
+    uint32_t integrator;
+    uint32_t debounced_pin_state;
+
+    int64_t long_press_period_ms;
+    int64_t short_press_period_ms;
+
+    int64_t push_timestamp_us;
+    int64_t release_timestamp_us;
+
+}button_exti_str;
+
+#define LATCH_SWITCH_PIN        GPIO_NUM_13
+#define DEBOUNCE_SAMPLES        10
+#define POLLING_PERIOD          5
+//Tasks
 
 void button_task(void *pvParameters)
 {
@@ -108,68 +86,261 @@ void button_task(void *pvParameters)
 
     button_exti_str btn_str = 
     {
-        .btn_task = &button_task_h,
-        .exti_pin = GPIO_NUM_5,
-        .debounce_period = 50,
-        .press_start_timestamp = 0,
-        .press_end_timestamp = 0,
-        .long_press_period = 5000,
-        .short_press_period = 200,
-        .pin_logic_level = 0,
+        .btn_pin = GPIO_NUM_13,
+        .integrator = 0,
+        .long_press_period_ms = 3000,
+        .short_press_period_ms = 200,
+        .debounce_samples = 10,
+        .polling_period_ms = 10,
     };
 
     gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_ANYEDGE,         // Interrupt on falling edge
-        .mode = GPIO_MODE_INPUT,                // Set as input
-        .pin_bit_mask = (1ULL << btn_str.exti_pin),// Bit mask for the pin
-        .pull_up_en = GPIO_PULLUP_ENABLE,       // Enable Internal Pull-Up (IPU)
-        .pull_down_en = GPIO_PULLDOWN_DISABLE   // Disable Pull-Down
+        .pin_bit_mask = (1ULL << btn_str.btn_pin),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,     // Assumes active-low wiring (Switch to GND)
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE        // STRICTLY DISABLED to prevent ISR storms
     };
     gpio_config(&io_conf);
 
-    gpio_install_isr_service(0);
-    ESP_ERROR_CHECK(gpio_isr_handler_add(btn_str.exti_pin, gpio_isr_handler, (void *)&btn_str));
-
-    uint32_t notify_val = 0x00;
     while (1)
     {
-        ESP_LOGI(BTN_TAG, "btn task tick");
-        BaseType_t btn_notify_ret = xTaskNotifyWait(0x00, ULONG_MAX, &notify_val, pdMS_TO_TICKS(1000));
-        if (btn_notify_ret == pdPASS)
+        // ESP_LOGI(BTN_TAG, "btn tick\r\n");
+        int pin_level = gpio_get_level(btn_str.btn_pin);
+        if (pin_level == 0)
         {
-            if (notify_val & BTN_NOTIFY_LONG_PRESS)
+            if (btn_str.integrator > 0)
             {
-                ESP_LOGI(BTN_TAG, "Short press detected, length = %lu\r\n", btn_str.last_press_length);
+                btn_str.integrator--;
             }
-            else if (notify_val & BTN_NOTIFY_SHORT_PRESS)
+        }
+        else
+        {
+            if (btn_str.integrator < btn_str.debounce_samples)
             {
-                ESP_LOGI(BTN_TAG, "Long press detected, length = %lu\r\n", btn_str.last_press_length);
+                btn_str.integrator++;
             }
-            else
+        }
+
+        if (btn_str.integrator == btn_str.debounce_samples && btn_str.debounced_pin_state == 0)
+        {
+            btn_str.debounced_pin_state = 1;
+            btn_str.push_timestamp_us = esp_timer_get_time();
+            // ESP_LOGI(BTN_TAG, "debounced button pushed, time = %lld\r\n", btn_str.push_timestamp_us);
+        }
+        else if (btn_str.integrator == 0 && btn_str.debounced_pin_state != 0)
+        {
+            btn_str.debounced_pin_state = 0;
+            btn_str.release_timestamp_us = esp_timer_get_time();
+            // ESP_LOGI(BTN_TAG, "debounced button released, time = %lld\r\n", btn_str.release_timestamp_us);
+            int64_t press_length_us = btn_str.release_timestamp_us - btn_str.push_timestamp_us;
+
+            if (press_length_us > btn_str.short_press_period_ms * 1000 && press_length_us < btn_str.long_press_period_ms * 1000)
             {
-                ESP_LOGW(BTN_TAG, "Unknown notify from btn ISR");
+                ESP_LOGI(BTN_TAG, "short press detected, length = %lld\r\n", press_length_us / 1000);
+
             }
+            else if (press_length_us >= btn_str.long_press_period_ms * 1000)
+            {
+                ESP_LOGI(BTN_TAG, "long press detected, length = %lld\r\n", press_length_us / 1000);
+            }
+        }
+
+        //freertos has 100Hz tickrate, polling period less than 10ms crashes the program
+        vTaskDelay(pdMS_TO_TICKS(btn_str.polling_period_ms));
+        // vTaskDelay(1);
+    }
+}
+
+typedef enum
+{
+    STATE_STARTUP = 0, 
+    STATE_INIT,
+    STATE_IDLE,
+    STATE_MEASURING,
+    STATE_WIFI_CONNECTING,
+    STATE_UPLOADING,
+    STATE_OTA_CHECKING,
+    STATE_OTA_UPDATING,
+    STATE_ERROR
+}LED_STATE_t;
+
+typedef enum
+{
+    STATE_CMD_STARTUP_TO_INIT = (uint32_t)(1 << 0),
+    STATE_CMD_INIT_TO_IDLE = (uint32_t)(1 << 1),
+    STATE_CMD_MEASURE = (uint32_t)(1 << 2),
+    STATE_CMD_MEASURE_TO_WIFI_CONNECTING = (uint32_t)(1 << 3),
+    STATE_CMD_WIFI_CONNECT_TO_UPLOADING = (uint32_t)(1 << 4),
+    STATE_CMD_OTA_START = (uint32_t)(1 << 5),
+    STATE_CMD_ERROR_RESET = (uint32_t)(1 << 31),
+}state_task_notify_cmd_t;
+
+typedef enum
+{
+    MEASURE_CMD_START = (1 << 10),
+    MEASURE_CMD_STOP = (1 << 11),
+    MEASURE_CMD_WIFI_CONNECT_CALLBACK = (1 << 12),
+    MEASURE_CMD_WIFI_UPLOAD_CALLBACK = (1 << 13),
+}measure_task_cmd_t;
+
+void led_task(void *pvParameters)
+{
+    ESP_LOGI(LED_TAG, "LED task started\r\n");
+
+    configure_led();
+
+    LED_STATE_t current_state = STATE_STARTUP;
+    uint32_t notify_val = 0;
+    while (1)
+    {
+        BaseType_t notify_ret = xTaskNotifyWait(0x00, ULONG_MAX, &notify_val, portMAX_DELAY);
+        if (notify_ret == pdPASS)
+        {
+            if (notify_val & STATE_CMD_STARTUP_TO_INIT && current_state == STATE_STARTUP)
+            {
+                ESP_LOGI(LED_TAG, "startup to init cmd\r\n");
+                current_state = STATE_INIT;
+            }
+            else if (notify_val & STATE_CMD_INIT_TO_IDLE && current_state == STATE_INIT)
+            {
+                ESP_LOGI(LED_TAG, "init to idle cmd\r\n");
+                current_state = STATE_IDLE;
+            }
+            else if (notify_val & STATE_CMD_MEASURE)
+            {
+                ESP_LOGI(LED_TAG, "measure cmd\r\n");
+                if (NULL == measure_task_h)
+                {
+                    ESP_LOGE(LED_TAG, "measure task not initialized");
+                }
+                else
+                {
+                    if (current_state == STATE_IDLE)
+                    {
+                        xTaskNotify(measure_task_h, MEASURE_CMD_START, eSetBits);
+                    }
+                    else if (current_state == STATE_MEASURING)
+                    {
+                        xTaskNotify(measure_task_h, MEASURE_CMD_STOP, eSetBits);
+                    }
+                }
+            }
+            else if (notify_val & STATE_CMD_MEASURE_TO_WIFI_CONNECTING)
+            {
+                //notification from measure task that it started wifi connection
+                current_state = STATE_WIFI_CONNECTING;
+                //WARNING might need to set LEDS here and give notification back to wifi process for having correct indication at the right time
+                xTaskNotify(measure_task_h, MEASURE_CMD_WIFI_CONNECT_CALLBACK, eSetBits);
+            }
+            else if (notify_val & STATE_CMD_WIFI_CONNECT_TO_UPLOADING)
+            {
+                //notification from measure task that it started data upload
+                current_state = STATE_UPLOADING;
+                xTaskNotify(measure_task_h, MEASURE_CMD_WIFI_UPLOAD_CALLBACK, eSetBits);
+            }
+            else if (notify_val & STATE_CMD_OTA_START)
+            {
+
+            }
+            else if (notify_val & STATE_CMD_ERROR_RESET)
+            {
+
+            }
+        }
+
+        switch (current_state)
+        {
+            case (STATE_INIT):
+            {
+                ESP_LOGI(LED_TAG, "State init, green blinking led\r\n");
+                gpio_set_level(BLINK_GPIO, 0);
+            }
+            break;
+            case (STATE_IDLE):
+            {
+                ESP_LOGI(LED_TAG, "State IDLE, green led\r\n");
+                gpio_set_level(BLINK_GPIO, 1);
+            }
+            break;
+            case (STATE_MEASURING):
+            {
+                ESP_LOGI(LED_TAG, "State Measuring, blue blinking led\r\n");
+            }
+            break;
+            case (STATE_WIFI_CONNECTING):
+            {
+                ESP_LOGI(LED_TAG, "State WIFI Connecting, blue led\r\n");
+            }
+            break;
+            case (STATE_UPLOADING):
+            {
+                ESP_LOGI(LED_TAG, "State Uploading data, yellow blinking led\r\n");
+            }
+            break;
+            case (STATE_OTA_CHECKING):
+            case (STATE_OTA_UPDATING):
+            {
+                ESP_LOGI(LED_TAG, "State OTA check/update, yellow led\r\n");
+            }
+            break;
+            case (STATE_ERROR):
+            {
+                ESP_LOGE(ERROR_TAG, "ERROR State, RED led\r\n");
+                if (notify_val & STATE_CMD_ERROR_RESET)
+                {
+                    current_state = STATE_IDLE;
+                }
+            }
+            break;
+            default:
+                break;
         }
     }
 }
 
+void measure_task(void *pvParameters)
+{
+    ESP_LOGI(LED_TAG, "Measure task started\r\n");
 
-
-
-
+    while (1)
+    {
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
 
 void app_main(void)
 {
     ESP_LOGI(INIT_TAG, "%s\r\n", version_string);
+    /* Configure the peripheral according to the LED type */
+
+    //LED Task is the main orchestrator task, so high priority
+    BaseType_t led_ret = xTaskCreatePinnedToCore(led_task, 
+                                                "led task", 
+                                                4096, 
+                                                NULL, 
+                                                10, 
+                                                &led_task_h, 
+                                                1);
+    if (led_ret == pdPASS)
+    {
+        ESP_LOGI(INIT_TAG, "led task created\r\n");
+    }
+    else
+    {
+        ESP_LOGE(INIT_TAG, "led task creation failed\r\n");
+        while (1);
+    }
+
+    xTaskNotify(led_task_h, STATE_INIT, eSetBits);
 
     BaseType_t btn_ret = xTaskCreatePinnedToCore(button_task, 
                                                 "Btn task", 
                                                 4096, 
                                                 NULL, 
-                                                5, 
+                                                7, 
                                                 &button_task_h, 
                                                 1);
-
     if (btn_ret == pdPASS)
     {
         ESP_LOGI(INIT_TAG, "btn task created\r\n");
@@ -177,7 +348,31 @@ void app_main(void)
     else
     {
         ESP_LOGE(INIT_TAG, "btn task creation failed\r\n");
+        while (1);
     }
+
+    //wifi task should be pinned to core 0 but am not sure
+    BaseType_t measure_ret = xTaskCreatePinnedToCore(measure_task, 
+                                                    "Btn task", 
+                                                    4096, 
+                                                    NULL, 
+                                                    5, 
+                                                    &measure_task_h, 
+                                                    0);
+    
+    if (measure_ret == pdPASS)
+    {
+        ESP_LOGI(INIT_TAG, "measure task created\r\n");
+    }
+    else
+    {
+        ESP_LOGE(INIT_TAG, "measure task creation failed\r\n");
+        while (1);
+    }
+
+    //INIT DONE, MOVE ON TO IDLE STATE
+    xTaskNotify(led_task_h, STATE_IDLE, eSetBits);
+
 
     return;
 }
